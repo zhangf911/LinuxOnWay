d63fecef8816c4d3a38a18d3e1fb3c752b5216c3
zhangf <zhangf911@gmail.com>
2012-12-11 20:35:35 +0800

    1、添加时间功能

---
+++ DataTime.cpp
@@ -0,0 +1,14 @@
+/*
+ * DataTime.cpp
+ *
+ *  Created on: 2012-12-11
+ *      Author: king
+ */
+#include "DataTime.h"
+
+unsigned int DataTime::CurrentTime()
+{
+
+}
+
+
---
+++ DataTime.h
@@ -0,0 +1,25 @@
+/*
+ * DataTime.h
+ *
+ *  Created on: 2012-12-11
+ *      Author: king
+ */
+
+#ifndef DATATIME_H_
+#define DATATIME_H_
+
+
+class DataTime
+{
+public:
+	DataTime();
+
+	~DataTime();
+
+public:
+	static unsigned int CurrentTime();
+
+
+};
+
+#endif /* DATATIME_H_ */
